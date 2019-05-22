d5a9c816df3d9677abe8c5dd644c911faeac19f8
FarhadSiddiqui <50422478+FarhadSiddiqui@users.noreply.github.com>
2019-05-23 00:34:31 +0500

    Add files via upload

---
+++ Assignment 9 .py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[46]:
+
+
+Freinds = ["Fahad", "Faisal", "Farhad" , "Ali", "Zohaib", "Fazeel", "Asad", 
+           "Asim", "Rizwan", "shayan", "aliyan", "Saim",  "Zain", "Babar", "Aliraza", 
+           "Saeed", "Arsalan", "Sohail" , "chaman","tanzil"]    
+CloseFreinds = ["Zain", "Saim", "Sohail" , "Arsalan" , "Tanzil", "Asad"]    
+Freinds.remove("Fahad", "Faisal", "Farhad" , "Ali", "Zohaib", "Fazeel",
+"Asim", "Rizwan", "shayan", "aliyan" ,"Babar", "Aliraza",   "Saeed",  "chaman")
+print (Freinds)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
